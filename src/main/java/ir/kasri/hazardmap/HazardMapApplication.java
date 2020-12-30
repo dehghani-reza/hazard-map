dacabb2022c72708453c8bcf39249c6a5390e7aa
Reza <dehghan7447@gmail.com>
2020-12-30 23:45:08 +0330

    h2 config

--- src/main/java/ir/kasri/hazardmap/HazardMapApplication.java
+++ src/main/java/ir/kasri/hazardmap/HazardMapApplication.java
@@ -2,10 +2,15 @@ package ir.kasri.hazardmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HazardMapApplication {
-
+public class HazardMapApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder HazardMapApplication) {
+		return HazardMapApplication.sources(HazardMapApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HazardMapApplication.class, args);
 	}
